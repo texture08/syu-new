65e2144cb7a88593ec785b40b931adc291c90c30
Syu <58730464+texture08@users.noreply.github.com>
2022-07-02 12:21:09 +0900

    Add files via upload

---
+++ scripts/header.js
@@ -0,0 +1,22 @@
+document.write(`<header>
+        <div class="header">
+                <div id="nav-drawer"><input id="nav-input" type="checkbox" class="nav-unshown"><label id="nav-open" for="nav-input"><span></span><span></span><span></span></label><label class="nav-unshown" id="nav-close" for="nav-input"></label>
+                        <div id="nav-content">
+                                <h3 class="title-1"><span class="mgr-10">お借りした素材など</span></h3>
+                                <p class="h"><span class="mgr-10">使用フォント</span><br><span class="mgr-10"><a href="http://font.sumomo.ne.jp/font_1.html" target="_blank" class="color1">けいふぉんと</a></span><br><span class="mgr-10"><a href="https://logotype.jp/corporate-logo-font-dl.html#i-10" target="_blank" class="color1">コーポレート・ロゴ</a></span></p>
+                                <p class="h"><span class="mgr-10">アイコン</span><br><span class="mgr-10"><a href="https://fontawesome.com/?from=io" target="_blank" class="color1">FontAwesome</a></span></p>
+                                
+                                <h3><span class="mgr-10"><a href="https://twitter.com/H2DH8K?ref_src=twsrc%5Etfw" class="twitter-follow-button" data-show-count="false">フォローする By H2DH4K</a><sccolor1pt async src="https://platform.twitter.com/widgets.js" charset="utf-8"></sccolor1pt></span></p>
+                        </div>
+                </div>
+                <div class="header-log">シューのリソース工房</div>
+                <div id="hedaer-list">
+                        <ul>
+                                <li><a class="color2" href="">ホーム</a></li>
+                                <li><a class="color2" href="">ブログ</a></li>
+                                <li><a class="color2" href="">ツール</a></li>
+                                <li><a class="color2" href="https://twitter.com/H2DH8K">開発者</a></li>
+                                </ul>
+                </div>
+        </div>
+</header>`);
