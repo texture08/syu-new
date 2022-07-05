27c12eec8a9f74e62cb7696d77b5e99a5bb35339
Syu <58730464+texture08@users.noreply.github.com>
2022-07-05 19:56:51 +0900

    Add files via upload

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,8 +1,8 @@
 document.write(`<footer>
-        <p>シューのリソース工房<span class="mgr-10">お問い合わせ</span></p>
-        <p><a href="https://twitter.com/H2DH8K" target="_blank" class="color1">Twitter</a> ID:H2DH8K<br> <a href="https://discord.gg/FuDaAWaNAV" target="_blank" class="color1">Discord</a> ID:シュー#8493<br> <span class="mini">&copy; 2021〜</span></p>
+        <p>${name}<span class="mgr-10">お問い合わせ</span></p>
+        <p><a href="https://twitter.com/H2DH8K" target="_blank" class="color1">Twitter</a> ID:H2DH8K<br> <a href="https://discord.gg/FuDaAWaNAV" target="_blank" class="color1">Discord</a> ID:シュー#8493<br> <span class="mini">&copy; 2022〜</span></p>
     </footer>
     <div class="foo">
-		<div class="card"><a class="twitter-timeline" data-width="1000" data-height="650" data-theme="dark" href="https://twitter.com/H2DH8K?ref_src=twsrc%5Etfw">Tweets by H2DH8K</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script></div>
+		<div class="cards"><a class="twitter-timeline" data-width="1000" data-height="650" data-theme="dark" href="https://twitter.com/H2DH8K?ref_src=twsrc%5Etfw">Tweets by H2DH8K</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script></div>
     </div>`);
 //<div ><a href="https://syu-rws.vercel.app/article/me.html"><img src="./img/me.png" id="me"></a></div>
